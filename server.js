e5c436e35e096eb5ce4668b5aa4c4d5fe162f96c
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2026-01-12 16:32:50 +0500

    doing work on backend

--- server.js
+++ server.js
@@ -9,10 +9,25 @@ config({
     path: "./.env"
 })
 
+// Global variables...!
+const port = process.env.PORT;
+const app = express();
+let todoBucket = []; // Data will be stored here...!
+
+// Middlewares...!
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
+
 // Create 1st api: / route...!
 app.get("/", (req, res) => {
   return res.status(200).send({
     statusCode: 200,
     message: "Welcome to Back End using Node JS",
   });
+});
+
+// Server running...!
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
